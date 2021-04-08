150ef64b21b585dfcad6d5d8aa7370c02b25af40
Alice Mazzoleni <alicemazzoleni@alices-macbook.home>
2021-04-08 09:34:50 +0200

    game controller

--- include/server/game_controller.hpp
+++ include/server/game_controller.hpp
@@ -2,7 +2,21 @@
 #define GAME_CONTROLLER_HPP
 
 #include "../common/common.hpp"
+#include "game_state.hpp"
+class game_controller
+{
+public:
+    game_controller()
 
+    void eval_request(Player_id, std::string&); //evaluate requests and send responses
+
+    void init(); //initialize game_state
+
+    void update_game_state();
+
+private:
+    game_state game_state_;
+};
 #endif /* GAME_CONTROLLER_HPP */
 
 
--- src/server/game_controller.cpp
+++ src/server/game_controller.cpp
@@ -1,18 +1,5 @@
 #include "../../include/server/game_controller.hpp"
 
-#include "game_state.hpp"
-
-
-class game_controller
-{
-public:
 
 
 
-
-
-
-private:
-	game_state game_state_;
-	
-};
