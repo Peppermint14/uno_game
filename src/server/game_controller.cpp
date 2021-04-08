3315b5ab152a858d8ca9d4bfbe31ca979c50a04d
Alice Mazzoleni <alicemazzoleni@alices-macbook.home>
2021-04-08 13:44:33 +0200

    some changes to game_controller.cpp

--- src/server/game_controller.cpp
+++ src/server/game_controller.cpp
@@ -1,5 +1,11 @@
 #include "../../include/server/game_controller.hpp"
 
+//constructor, constructs game_state
+game_controller::game_controller()
+{
+    game_state_ = new game_state();
+    game_state_->discard_pile
 
+}
 
 
