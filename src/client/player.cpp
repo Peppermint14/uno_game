c131d40937313723f3aaec2d99d964c6fa6e3dfd
kgutmann <kgutmann@student.ethz.ch>
2021-04-19 15:54:35 +0200

    added a few functions in the player.cpp

--- src/client/player.cpp
+++ src/client/player.cpp
@@ -1,3 +1,29 @@
-
 #include "../../include/client/player.hpp"
 
+
+Player::Player(Player_id player_id_, bool is_active_)
+{
+	player_id = player_id_;
+	is_active = is_active_;
+	player_state = new Player_state();
+}
+
+void Player::set_is_active(bool active)
+{
+	is_active = active; 	
+}
+
+Player_id Player::get_player_id() const
+{
+	return player_id;
+}
+
+Player_State* Player::get_player_state() const
+{
+	return player_state;
+}
+        
+bool Player::get_is_active() const
+{
+	return is_active;
+}
