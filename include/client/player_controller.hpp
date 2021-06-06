f781491ee77a7376c4bc6ea03c81fc26267b9970
kgutmann <kgutmann@student.ethz.ch>
2021-06-06 08:23:39 +0000

    Update player_controller.hpp

--- include/client/player_controller.hpp
+++ include/client/player_controller.hpp
@@ -14,17 +14,24 @@
 class player_controller {
 
 public:
-    // player_controller* get_ctrl(){return this;};
+
     static void init(GameWindow* gameWindow);
+    //processes all the information from the connection panel and calls the TCCP_Clpient connect function
     static void connectToServer();
+    //handles all the responses from the server
     static void eval_response(const std::string& msg);
-    //static void updatePlayerState(Player_State* newPlayerState);
+    //updates the graphical interface
     static void updatePlayerState();
     static void startGame();
     static void drawCard();
     static void fold();
     static void PickColour();
+    
+    //sends request to server, that the client wants to play the card cardToPlay
     static void playCard(const ck_Cards::Cards* cardToPlay);
+
+    // will be called right after receiving the Id from the server 
+    // sends the player name with the ID of the player to the server, to inform the server about the name
     static void join();
     static void exit();
 
@@ -39,28 +46,21 @@ public:
     //in: Id of the player, of which you want to know the number of cards
     //out: integer telling how many cards the corresponding player has
     static std::list<std::pair<Player_id, int>> get_number_cards_player();
-    
-    // void set_current_player(Player_id);
-    // Player_id get_current_player();
 
     static void set_color(ck_Cards::Color);
     static ck_Cards::Color get_color();
 
-    //static void set_top_card_discardp(ck_Cards::Cards);
     static ck_Cards::Cards get_top_card_discardp();
-
-    //static void set_error_message(std::string);
-    //static void error_read(); //-> error_message is not accurate anymore
-    //std::string get_error_message();
+    //helper function used to convert the wxString, which is returned when a player choses a color, to a value of type ck_Cards::Color 
     static ck_Cards::Color wxStr_to_Color(wxString color);
 //private:
     static GameWindow* _gameWindow ;
     static ConnectionPanel* _connectionPanel;
     static MainGamePanel* _mainGamePanel;
-    // static player_controller* _this_ctrl;
     //at position i is the number of cards that player i has stored (position 0 is zero (player zero is error)
     static std::list<std::pair<Player_id, int>> players_number_of_cards;
     static Player* _me;
+    //Player ID of player whose turn it is at the moment
     static Player_id current_player;
     static Player_State* _currentPlayerState;
     static ck_Cards::Color color_to_be_played; 
